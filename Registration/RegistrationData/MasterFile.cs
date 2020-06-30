caccd7c641e9ad776b44e0c609cd07cd83d9f33d
CorruptedLink01 <marcel.mathiss@gmail.com>
2020-06-30 10:57:02 +0200

    Started RegistrationData

---
+++ Registration/RegistrationData/MasterFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationData
+{
+    [Table("master_file")]
+    public class MasterFile : CreatedModify
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("firstname")]
+        public string Firstname { get; set; }
+        [Column("lastname")]
+        public string Lastname { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("sv_nr")]
+        public int? SVNumber { get; set; }
+
+        [Column("geb_date")]
+        public DateTime? Birthday { get; set; }
+
+        [Column("gender")]
+        public string Gender { get; set; }
+
+        [Column("busy")]
+        public string Busy { get; set; }
+
+        [Column("busy_by")]
+        public string BusyAt { get; set; }
+
+        [Column("picture")]
+        public string Picture { get; set; }
+
+        public string Function { get; set; }
+
+    }
+}
---
+++ Registration/RegistrationData/PersonEntities.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationData
+{
+    public class PersonEntities : DbContext
+    {
+        [Column("created@")]
+        public DateTime CreatedAt{ get; set; }
+
+        [Column("modify@")]
+        public DateTime ModifiedAt { get; set; }
+    }
+}
