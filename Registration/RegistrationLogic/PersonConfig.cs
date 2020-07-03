683da063c6fa31d4012f1b7bd57c25d829aa0e25
CorruptedLink01 <marcel.mathiss@gmail.com>
2020-07-03 12:28:36 +0200

    Added AddPeopleFromCSV

---
+++ Registration/RegistrationLogic/PersonConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationLogic
+{
+    public class PersonConfig
+    {
+
+        public int Name1 { get; set; }
+
+        public int Name2 { get; set; }
+
+        public int? Title { get; set; }
+
+        public int? SvNumber { get; set; }
+
+        public int? Date { get; set; }
+
+        public int? Gender { get; set; }
+
+        //{ "name1": "1", "name2": 2}
+
+    }
+}
